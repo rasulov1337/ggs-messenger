002755d792d382f34f3d7a71e631de049a5e42e6
k1tten2005 <145349824+k1tten2005@users.noreply.github.com>
2024-06-07 20:39:11 +0300

    add chat participants and messages models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
+class Profiles(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=70)
     # avatar = models.ImageField(upload_to='uploads/images', default='default-avatar.png')
@@ -22,3 +22,21 @@ class Chats(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ChatParticipants(models.Model):
+    chat = models.OneToOneField(Chats, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile.user.username
+
+
+class Messages(models.Model):
+    chat = models.ForeignKey(Chats, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
