6a7de74ab6e86f873ab4620008b705db9192f717
k1tten2005 <145349824+k1tten2005@users.noreply.github.com>
2024-06-07 21:08:02 +0300

    edit models' names and add max length constraint to message

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profiles(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=70)
     # avatar = models.ImageField(upload_to='uploads/images', default='default-avatar.png')
@@ -16,7 +16,7 @@ class Profiles(models.Model):
         return self.name
 
 
-class Chats(models.Model):
+class Chat(models.Model):
     name = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -24,18 +24,18 @@ class Chats(models.Model):
         return self.name
 
 
-class ChatParticipants(models.Model):
-    chat = models.OneToOneField(Chats, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+class ChatParticipant(models.Model):
+    chat = models.OneToOneField(Chat, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.profile.user.username
 
 
-class Messages(models.Model):
-    chat = models.ForeignKey(Chats, on_delete=models.CASCADE)
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.TextField(max_length=500)
     sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
