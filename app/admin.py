e73815ff2b26f7f0270fa373865ca26c8e862f2f
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-06-10 21:15:18 +0300

    Register models to admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Profile)
+admin.site.register(models.Message)
+admin.site.register(models.Chat)
+admin.site.register(models.ChatParticipant)
