e0d286bff9143841862dada12682cc1d8af9a951
k1tten2005 <145349824+k1tten2005@users.noreply.github.com>
2024-06-11 11:57:28 +0300

    refactor code

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('auth/register', views.register, name='register'),
     path('auth/logout', views.logout, name='logout'),
     path('chats', views.get_chats, name='get-chats'),
-    path('chats/search', views.search_chats, name='search-chat'),
+    path('chats/search', views.search_chats, name='search-chats'),
     path("", views.index, name="index"),
 ]
