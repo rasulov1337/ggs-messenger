c57a5c93ec4b73d7cb0af201bca7292615a1f873
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-06-14 10:42:47 +0300

    Fix migrations

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "${0}: running migrations."
+# python manage.py makemigrations --merge
+python manage.py migrate --noinput
+
+# echo "${0}: collecting statics."
+
+# python manage.py collectstatic --noinput
+
+# cp -rv static/* static_shared/
+
+# gunicorn yourapp.wsgi:application \
+    # --env DJANGO_SETTINGS_MODULE=yourapp.production_settings \
+    # --name yourapp \
+    # --bind 0.0.0.0:8000 \
+    # --timeout 600 \
+    # --workers 4 \
+    # --log-level=info \
+    # --reload  # Only for development
+python manage.py runserver 0.0.0.0:8000
